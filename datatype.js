0c47c871fab4b6a757e92eaa0e64a88c36e115e7
Sagar-S16 <sagarcheeku18@gmail.com>
2025-06-17 07:39:43 +0000

    datatypes

---
+++ datatype.js
@@ -0,0 +1,26 @@
+"use strict"// treat all JS code as newer version
+
+// alert(3+3) we are using node js, not browser
+
+console.log(3+3)
+console.log("Sagar")
+
+
+let name = "SAGAR"
+let age  = 18
+let isLoggedIn = false
+let state = null
+
+//Primitive datatypes
+// number => 2 to power 53
+// bigint
+// String ""
+// boolean true/false
+// null -- standalone value typeof - object
+// undefined                typeof - Undefined
+// symbol uniqueness
+
+//non - Primitive datatypes
+//object 
+
+console.log(typeof"Sagar")
