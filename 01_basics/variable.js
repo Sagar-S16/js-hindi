f8cec0defeff888daf85f2068087ce9939b905ea
Sagar-S16 <sagarcheeku18@gmail.com>
2025-06-17 07:13:08 +0000

    datatypes

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 1445533
+let accountEmail = "sagar@google.com"
+var accountPassword = "!2345"
+accountCity = "Bangalore"
+let accountState;
+/*
+    var variable are often not used due to block and functional scope issue
+*/
+accountEmail = "sagar@com"
+accountPassword = "2345"
+accountCity = "Hassan"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
