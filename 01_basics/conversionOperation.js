e75f686e0b29b6cef0c5b2d665cb6bddfcb4f464
Sagar-S16 <sagarcheeku18@gmail.com>
2025-06-18 07:09:14 +0000

    conversion

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -27,4 +27,9 @@ let stringNumber = String(someNumber)
 // console.log(stringNumber);
  console.log(typeof stringNumber);
 
+ //operations
+
+ let value = 3
+ let negValue = -value
+ console.log(negValue)
 
